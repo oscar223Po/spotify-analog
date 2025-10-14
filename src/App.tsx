411e4900cca2aa9eaec2add9ae65ca7478150b09
Oscar <oscaritoparapito225@mail.ru>
2025-10-14 14:51:55 +0600

    updates: change image in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Play } from 'lucide-react'
+import image from '../public/banner.jpg'
 import { SearchField } from './components/elements/search-field/SearchField'
 import { Track } from './components/elements/track-item/Track'
 import { TRACKS } from './data/tracks.data'
+
 
 function App() {
 	return (
@@ -9,11 +11,10 @@ function App() {
 			<SearchField />
 			<div className="relative">
 				<img
-					src="/banner.jpg"
-					alt=""
-					className="rounded-xl w-full"
+					src={image}
+					alt="Banner"
+					className="rounded-xl"
 				/>
-
 				<div className="flex items-center justify-between absolute bottom-layout left-0 w-full px-layout">
 					<div>
 						<h1 className="text-2xl font-semibold mb-[0.18rem] text-white">
